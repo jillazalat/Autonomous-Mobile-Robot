4f07df100a5ac8536d3c245a4a1b58bc7b83f01b
Fazle99 <93679303+Fazle99@users.noreply.github.com>
2021-11-26 12:55:50 -0500

    Update P2_Fazle.ino

--- P2_Fazle/P2_Fazle.ino
+++ P2_Fazle/P2_Fazle.ino
@@ -1,67 +1,100 @@
-#include <IRremote.h>
-
+#include <IRremote.h>           //Include libraries
 #include <Stepper.h>
 #define STEPS 2048
-Stepper stepper(STEPS, 9, 11, 10, 12);
-
 #include <Servo.h>
+
+Stepper stepper(STEPS, 9, 11, 10, 12);  //Initialize Stepper and Servo
 Servo myservo;
 
-const int RECV_PIN = 13;
+int trig = 4;                 // trig pin for HC-SR04
+int echo = 3;                 // echo pin for HC-SR04
+float duration, distance;
+
+const int RECV_PIN = 13;      //Initialize IR Receiver
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 unsigned long key_value = 0;
 
 void setup() {
-   Serial.begin(9600);
-   stepper.setSpeed(10);
-   myservo.attach(6);
-   irrecv.enableIRIn();
+  Serial.begin(9600);
+  stepper.setSpeed(10);      //Set Stepper speed
+  myservo.attach(6);         //Set Servo pin
+  irrecv.enableIRIn();
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
 }
 
 void loop() {
-   int pos=constrain(pos,50,140);
-   if (irrecv.decode(&results)){
- 
-        if (results.value == 0XFFFFFFFF)
-          results.value = key_value;
+  int pos = constrain(pos, 60, 144); //define servo limits for steering
+  if (irrecv.decode(&results)) {
 
-        switch(results.value){
-          case 0xFFA25D:
-          Serial.println("ON / OFF");
-          stepper.step(-14500);
-          myservo.write(140);
-          stepper.step(-9000);
-          myservo.write(102);
-          stepper.step(-8000);
-          myservo.write(50);
-          stepper.step(-10000);
-          myservo.write(102);
-          stepper.step(-10000);
-          break;
-          case 0xFF629D:
-          Serial.println("VOL+");
-          stepper.step(-1000);
-          break;
-          case 0xFFE21D:
-          Serial.println("FUNC / STOP");
-          myservo.write(102);
-          break;
-          case 0xFF22DD:
-          Serial.println("|<<");
-          myservo.write(50);
-          break;
-          case 0xFF02FD:
-          Serial.println(">||");
+    if (results.value == 0XFFFFFFFF)    //define IR Remote buttons and corresponding actions
+      results.value = key_value;
+
+    switch (results.value) {
+      case 0xFF18E7:                 //Task 2, motion in a predefined path
+        Serial.println("2");
+        stepper.step(14500);
+        myservo.write(144);
+        stepper.step(9000);
+        myservo.write(102);
+        stepper.step(8000);
+        myservo.write(60);
+        stepper.step(10000);
+        myservo.write(102);
+        stepper.step(10000);
+        break;
+      case 0xFF629D:                //Manual motion Forward
+        Serial.println("VOL+");
+        stepper.step(1000);
+        break;
+      case 0xFF30CF:                //Manual motion wheels straight
+        Serial.println("1");
+        myservo.write(102);
+        break;
+      case 0xFF22DD:                //Manual motion wheels Left
+        Serial.println("|<<");
+        myservo.write(60);
+        break;
+      case 0xFFA857:                //Manual motion Backward
+        Serial.println("VOL-");
+        stepper.step(-1000);
+        break ;
+      case 0xFFC23D:                //Manual motion wheels Right
+        Serial.println(">>|");
+        myservo.write(144);
+        break ;
+      case 0xFF7A85:                //Task 3
+        Serial.println("3");
+        if (distance > 10) {
+          stepper.step(12000);
+        } else {
+          myservo.write(144);
           stepper.step(1000);
-          break ;  
-          case 0xFFC23D:
-          Serial.println(">>|");
-          myservo.write(140);
-          break ;               
-               
+          myservo.write(60);
+          stepper.step(1000);
+          myservo.write(102);
+          stepper.step(5000);
+          myservo.write(60);
+          stepper.step(1000);
+          myservo.write(144);
+          stepper.step(1000);
+          myservo.write(102);
         }
+        break;
         key_value = results.value;
-        irrecv.resume(); 
+        irrecv.resume();
+    }
   }
+}
+
+float dist_calc(int pos) {  // trigger 40kHz pulse for ranging
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  // convert from duration for pulse to reach detector (microseconds) to range (in cm)
+  duration = pulseIn(echo, HIGH); // duration for pulse to reach detector (in microseconds)
+  distance = 100.0 * (343.0 * (duration / 2.0)) / 1000000.0; // 100.0*(speed of sound*duration/2)/microsec conversion
 }
