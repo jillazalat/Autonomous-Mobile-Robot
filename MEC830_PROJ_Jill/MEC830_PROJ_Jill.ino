189527358a8274538e32b4768caee11e54caefe9
Jill Aghyourli Zalat <39888814+jaghz@users.noreply.github.com>
2021-11-27 21:07:58 -0500

    Create MEC830_PROJ_Jill.ino

---
+++ MEC830_PROJ_Jill/MEC830_PROJ_Jill.ino
@@ -0,0 +1,68 @@
+#include <IRremote.h>
+
+#include <Stepper.h>
+#define STEPS 2048
+Stepper stepper(STEPS, 8, 10, 9, 11);
+
+#include <Servo.h>
+Servo myservo;
+
+const int RECV_PIN = 7;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+unsigned long key_value = 0;
+
+void setup() {
+   Serial.begin(9600);
+   stepper.setSpeed(10);
+   myservo.attach(5);
+   irrecv.enableIRIn();
+   myservo.write(75);
+}
+
+void loop() {
+   int pos=constrain(pos,45,120);
+   if (irrecv.decode(&results)){
+ 
+        if (results.value == 0XFFFFFFFF)
+          results.value = key_value;
+
+        switch(results.value){
+          case 0xFFA25D:
+//          Serial.println("ON / OFF");
+//          stepper.step(-15000);
+//          myservo.write(120);
+//          stepper.step(-7300);
+//          myservo.write(90);
+//          stepper.step(-8000);
+//          myservo.write(60);
+//          stepper.step(-8300);
+//          myservo.write(90);
+//          stepper.step(-15000);
+          break;
+          case 0xFF629D: // Move Forward
+          Serial.println("VOL+");
+          stepper.step(1000);
+          break;
+          case 0xFFE21D: // Straighten Wheels
+          Serial.println("FUNC / STOP");
+          myservo.write(75);
+          break;
+          case 0xFF22DD: // Left Turn
+          Serial.println("|<<");
+          myservo.write(45);
+          break;
+          case 0xFF02FD: 
+          Serial.println(">||");
+          stepper.step(1000);
+          break ;  
+          case 0xFFC23D:
+          Serial.println(">>|"); // Right Turn
+          myservo.write(120);
+          break ;               
+               
+        }
+        key_value = results.value;
+        irrecv.resume(); 
+  }
+}
