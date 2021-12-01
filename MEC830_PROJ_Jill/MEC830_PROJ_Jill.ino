e93d276d66105a6b105e122dfaa7b95a9cbd5f4b
Jill Aghyourli Zalat <39888814+jaghz@users.noreply.github.com>
2021-11-30 20:40:35 -0500

    Update MEC830_PROJ_Jill.ino

--- MEC830_PROJ_Jill/MEC830_PROJ_Jill.ino
+++ MEC830_PROJ_Jill/MEC830_PROJ_Jill.ino
@@ -24,6 +24,10 @@ void setup() {
    myservo.write(90);
 }
 
+int straight = 96;
+int right = 45;
+int left = 130;
+int walk = 1000;
 void loop() {
   // int pos=constrain(pos,30,110);
    if (irrecv.decode(&results)){
@@ -34,34 +38,143 @@ void loop() {
     if (results.value == 0xFF629D) // Move Forward
     {
       Serial.println("VOL+");
-      stepper.step(-1000);
+      stepper.step(-walk);
     }
     if (results.value == 0xFFE21D)// Straighten Wheels
     {
       Serial.println("FUNC / STOP");
-      myservo.write(90);
+      myservo.write(straight);
     }
     if (results.value == 0xFF22DD)// Left Turn
     {
       Serial.println("|<<");
-      myservo.write(50);
+      myservo.write(left);
     }
     if (results.value == 0xFFC23D)// Right Turn
     {
       Serial.println(">>|");
        Serial.println("|<<");
-      myservo.write(130);
+      myservo.write(right);
 
     }
     if (results.value == 0xFFA857)// Move Backward
     {
        Serial.println("VOL-");
-      stepper.step(1000);
+      stepper.step(walk);
+    }
+    if (results.value == 0xFF30CF)
+    {
+      stepper.step(-8*walk);
+      
+      myservo.write(left); 
+      stepper.step(-walk);
+      
+      myservo.write(straight);
+      stepper.step(-4*walk);
+      
+      myservo.write(right);
+      stepper.step(-6*walk);
+
+//      myservo.write(left);
+//      stepper.step(-2*walk);
+
+      myservo.write(straight);
+      stepper.step(-8*walk);
+
+      myservo.write(right);
+      stepper.step(-4*walk);
+
+      myservo.write(straight);
+      stepper.step(-3*walk);
+
+//      myservo.write(left);
+//      stepper.step(-2*walk);
+
+      myservo.write(straight);
+      stepper.step(-5*walk);
+
+      myservo.write(right);
+      stepper.step(-4*walk);
+
+      myservo.write(straight);
+      stepper.step(-14*walk);
+
+//      stepper.step(-6*1000);
+//      myservo.write(45);
+//      stepper.step(-10*1000);
+//      myservo.write(130);
+//      stepper.step(-0.5*1000);
+//      myservo.write(90);
+//      
+//      stepper.step(-6*1000);
+//      myservo.write(45);
+//      stepper.step(-10*1000);
+//      myservo.write(130);
+//      stepper.step(-0.5*1000);
+//      myservo.write(90);
+//      stepper.step(-6*1000);
+      
+//       Serial.println("Performing Task 1");
+//       stepper.step(-9*1000);
+//       myservo.write(50);
+//       delay(100);
+//       stepper.step(-2*1000);
+//       myservo.write(90);
+//       delay(100);
+//       stepper.step(-3*1000);
+//        myservo.write(50);
+//        delay(100);
+//        stepper.step(-8*1000);
+//        delay(100);
+//        myservo.write(90);
+//        delay(100);
+//        stepper.step(-9*1000);
+//        delay(100);
+//        myservo.write(130);
+//        delay(100);
+//        stepper.step(-6*1000);
+//         delay(100);
+//        myservo.write(90);
+//        delay(100);
+//        stepper.step(-12*1000);
+////        delay(100);
+////        myservo.write(50);
+////       delay(100);
+////        stepper.step(-3*1000);
+////        delay(100);
+////        stepper.step(-3*1000);
+//        delay(100);
+//       Serial.println("Task 1 Complete");
     }
       
         key_value = results.value;
         irrecv.resume(); 
   }
   Serial.println (results.value, HEX); 
-  Serial.print(sonar.ping_cm());
+ // Serial.print(sonar.ping_cm());
+}
+void moveAlongFirstSegment() {
+  stepper.step(-7*walk);
+  myservo.write(left);
+  stepper.step(-3*walk);
+  myservo.write(straight);
+  stepper.step(-walk);
+  
+
+ 
+}
+void moveAlongSegment() {
+  stepper.step(-6.5*walk);
+  myservo.write(straight);
+  
+
+ 
+}
+void turnCornerRight() {
+      myservo.write(right);
+      stepper.step(-7*walk);
+      myservo.write(left);
+      stepper.step(-0.5*walk);
+      myservo.write(straight);
+//      stepper.step(-6*walk);
 }
