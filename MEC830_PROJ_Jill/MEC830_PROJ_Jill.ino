00977a2034c0ac4936072aa92a03128a7d15ef64
Jill Aghyourli Zalat <39888814+jaghz@users.noreply.github.com>
2021-12-01 01:10:15 -0500

    Update MEC830_PROJ_Jill.ino

--- MEC830_PROJ_Jill/MEC830_PROJ_Jill.ino
+++ MEC830_PROJ_Jill/MEC830_PROJ_Jill.ino
@@ -16,18 +16,20 @@ const int RECV_PIN = 7;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 unsigned long key_value = 0;
+int distance, flag=0;
+long totaldistance = 38000;
+int straight = 96;
+int right = 65;
+int left = 130;
+int walk = 1000;
 void setup() {
    Serial.begin(9600);
    stepper.setSpeed(15);
    myservo.attach(6);
    irrecv.enableIRIn();
-   myservo.write(90);
+   myservo.write(straight);
 }
 
-int straight = 96;
-int right = 45;
-int left = 130;
-int walk = 1000;
 void loop() {
   // int pos=constrain(pos,30,110);
    if (irrecv.decode(&results)){
@@ -62,16 +64,11 @@ void loop() {
        Serial.println("VOL-");
       stepper.step(walk);
     }
-    if (results.value == 0xFF30CF)
+    if (results.value == 0xFF30CF) // Task 4/Task 2
     {
-      stepper.step(-8*walk);
+      stepper.step(-12*walk);
       
-//      myservo.write(left); 
-//      stepper.step(-walk);
-      
-//      myservo.write(straight);
-//      stepper.step(-4*walk);
-      
+ 
       myservo.write(right);
       stepper.step(-6*walk);
 
@@ -79,20 +76,44 @@ void loop() {
       myservo.write(straight);
       stepper.step(-8*walk);
 
-      myservo.write(right);
-      stepper.step(-4*walk);
+      myservo.write(left);
+      stepper.step(-6*walk);
 
       myservo.write(straight);
-      stepper.step(-8*walk);
-
-      myservo.write(right);
-      stepper.step(-4*walk);
-
-      myservo.write(straight);
-      stepper.step(-8*walk);
-
+      stepper.step(-12*walk);
     }
-      
+    if (results.value == 0xFF18E7)
+     {
+      for (long i = 0; i <= totaldistance; i = i + 500) {
+          distance = sonar.ping_cm();
+              Serial.println(distance);
+          if (distance > 40) {              //If no object detected
+            stepper.step(-500);
+            Serial.println(i);
+          } 
+            if ((flag==0)&&(distance<40)){ //If object detected
+            myservo.write(right);
+            stepper.step(-3.5*walk);
+
+            myservo.write(left);
+            stepper.step(-2*walk);
+
+            myservo.write(straight);
+            stepper.step(-3*walk);
+
+            myservo.write(left);
+            stepper.step(-4*walk);
+            myservo.write(right);
+            stepper.step(-2.2*walk);
+ 
+            myservo.write(straight);
+            
+            i = i + 10000;
+            flag=1;
+            Serial.println(i);
+          }
+        }
+     }
         key_value = results.value;
         irrecv.resume(); 
   }
